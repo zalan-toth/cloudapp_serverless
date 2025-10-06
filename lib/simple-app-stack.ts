a2123bed60fb7dbebca839de3ae4dcfbcd052ee7
Zalán Tóth <zalant.tsc@gmail.com>
2025-10-06 01:32:46 +0100

    Create movies table and seed it.

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -1,6 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as custom from "aws-cdk-lib/custom-resources";
+import { generateBatch } from "../shared/util";
+import {movies} from "../seed/movies";
+
 
 import { Construct } from 'constructs';
 
@@ -20,6 +25,28 @@ export class SimpleAppStack extends cdk.Stack {
       cors: {
         allowedOrigins: ["*"],
       },
+      
+    });
+    const moviesTable = new dynamodb.Table(this, "MoviesTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: "id", type: dynamodb.AttributeType.NUMBER },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: "Movies",
+    });
+    new custom.AwsCustomResource(this, "moviesddbInitData", {
+      onCreate: {
+        service: "DynamoDB",
+        action: "batchWriteItem",
+        parameters: {
+          RequestItems: {
+            [moviesTable.tableName]: generateBatch(movies),
+          },
+        },
+        physicalResourceId: custom.PhysicalResourceId.of("moviesddbInitData"), //.of(Date.now().toString()),
+      },
+      policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [moviesTable.tableArn],
+      }),
     });
 
     new cdk.CfnOutput(this, "Simple Function Url", { value: simpleFnURL.url });
---
+++ shared/util.ts
@@ -0,0 +1,18 @@
+
+import { marshall } from "@aws-sdk/util-dynamodb";
+import { Movie } from "./types";
+
+export const generateMovieItem = (movie: Movie) => {
+  return {
+    PutRequest: {
+      Item: marshall(movie),
+    },
+  };
+};
+
+export const generateBatch = (data: Movie[]) => {
+  return data.map((e) => {
+    return generateMovieItem(e);
+  });
+};
+
